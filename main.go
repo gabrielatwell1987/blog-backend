14e1889d0a6b24774b68e7ca833549df5d081fd3
gabrielatwell1987 <gabrielatwell@proton.me>
2026-05-31 11:00:25 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"blog/go-backend/database"
+	"blog/go-backend/handlers"
+)
+
+func main() {
+    if err := database.InitDB("./blog.db"); err != nil {
+        log.Fatal(err)
+    }
+    defer database.DB.Close()
+
+    mux := http.NewServeMux()
+    mux.HandleFunc("GET /api/posts", handlers.GetPosts)
+    mux.HandleFunc("GET /api/posts/{id}", handlers.GetPost)
+    mux.HandleFunc("POST /api/posts", handlers.CreatePost)
+    mux.HandleFunc("PUT /api/posts/{id}", handlers.UpdatePost)
+    mux.HandleFunc("DELETE /api/posts/{id}", handlers.DeletePost)
+
+    handler := handlers.CORSMiddleware(mux)
+
+    log.Println("Server listening on :8080")
+    log.Fatal(http.ListenAndServe(":8080", handler))
+}
